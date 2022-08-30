0c4a5b0e14c99ddb6f4389a15417ddd76512960a
Syam <hey@syam.dev>
2022-08-30 15:33:21 +0530

    fix keys

--- kites_api/settings.py
+++ kites_api/settings.py
@@ -15,6 +15,8 @@ SECRET_KEY = os.environ.get("SECRET", default="django-insecure-jow#rf4lx#f6-el@i
 
 DEBUG = os.environ.get("DEBUG", default=True)
 ALLOWED_HOSTS = ["*"]
+AWS_S3_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_S3_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
