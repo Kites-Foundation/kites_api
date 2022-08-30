14034b2f246c217b58b34a2c8b7e7cb170e1736b
Syam <hey@syam.dev>
2022-08-30 23:42:50 +0530

    changes for db user

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # start.sh
 python manage.py collectstatic --noinput
-python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 gunicorn kites_api.wsgi:application --bind 0.0.0.0:8000 --workers 3
