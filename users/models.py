9461d4211023d27baf77ba5eaef60c0b19ebc6e8
Syam <hey@syam.dev>
2022-08-30 22:58:41 +0530

    minor change

--- users/models.py
+++ users/models.py
@@ -143,7 +143,8 @@ class User(AbstractUser):
     subject = models.CharField(max_length=100, default='', verbose_name="Subject ( major )", blank=True)
     occupation = models.CharField(max_length=200, verbose_name='Occupation', default='', blank=True)
     gender = models.CharField(max_length=50, default='', blank=True, choices=gender, verbose_name="Gender")
-    date_of_birth = models.DateTimeField(blank=True, verbose_name="Date Of Birth", null=True)
+    date_of_birth = models.DateTimeField(blank=True, verbose_name="Date Of Birth", default="2022-08-30 "
+                                                                                           "17:27:24.159743+00")
 
     current_address = models.TextField(max_length=200, default='', blank=True, verbose_name="Current Address")
     state_current = models.CharField(max_length=100, choices=states, default='', blank=True,
